da1a7d0575c12f73e4efaa643b98a1078f84f4e6
koratkar <woohooyeshaan@gmail.com>
2020-11-10 10:32:22 -0600

    yeet

--- app.js
+++ app.js
@@ -3,3 +3,32 @@
 //rs-25 is the name of a rocket engine
 //and I said biiiiiitch.
 
+let app = require('express')()
+let bodyParser = require('body-parser')
+let fs = require('fs')
+
+function songscan() {
+    let songs = fs.readdirSync(__dirname + "/songs", "utf8")
+    return songs.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
+}
+
+function recursive(arr, string, n) {
+    if (n == arr.length) {
+        return string
+    } else {
+        let astr = "then" + arr[n]
+        let en = n + 1
+        recursive(arr, astr, en)
+    }
+}
+
+app.get('/', (req, res) => {
+
+    res.send()
+})
+
+app.listen(3000, () => {
+    console.log("port 3000. alive. cool.")
+    console.log(recursive(["hee", "feeq", "beeeq", "hexagon"], "", 0))
+})
+
